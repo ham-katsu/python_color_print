53abc904ba44cff02ddb1d2223aca422e5f14282
R.Mizuhara <ham_katsu@outlook.jp>
2016-04-27 19:52:13 +0900

    make sample

---
+++ sample/sample.py
@@ -0,0 +1,37 @@
+from color import printc
+
+str1 = 'hello'
+str2 = 'world!'
+printc(str1+str2)
+
+printc(str1, "cri", False)
+printc(str2, "hcri")
+
+printc(str1, "gra", False)
+printc(str2, "hgr")
+
+printc(str1, "re", False)
+printc(str2, "hre")
+
+printc(str1, "yel", False)
+printc(str2, "hyel")
+
+printc(str1, "blu", False)
+printc(str2, "hblue")
+
+printc(str1, "mag", False)
+printc(str2, "hmag")
+
+printc(str1, "cy", False)
+printc(str2, "hcy")
+
+printc(str1, "gre", False)
+printc(str2, "hgreen")
+
+
+printc("if color name invalid","hogehoge")
+
+
+
+
+
